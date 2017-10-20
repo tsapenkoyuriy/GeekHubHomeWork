d48eda157dc8c1b13269797d5eb78606b7fdd147
tsapenkoy <yuriy.tsapenko.55@gmail.com>
2017-10-20 16:24:25 +0300

    init commit

---
+++ Lesson1Task3/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int factorial(int number){
+        int fact = 1;
+        if (number>0){
+            if (number>10) {
+                System.out.println("Calculating process will take some time");
+            }
+
+            for(int i=1;i<=number;i++){
+                fact*=i;
+            }
+            return fact;
+        }
+        else return 0;
+    }
+    public static void main(String[] args){
+       System.out.print("Введите число от которого вы желаете получить факториал:");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println("факториал числа "+number+": "+ factorial(number));
+    }
+}
