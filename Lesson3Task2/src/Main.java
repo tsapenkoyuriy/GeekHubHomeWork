c55ad69f6373f18210ffce6051fd0f5269dfb292
tsapenkoy <yuriy.tsapenko.55@gmail.com>
2017-11-03 16:14:37 +0200

    init commit

--- Lesson3Task2/src/Main.java
+++ Lesson3Task2/src/Main.java
@@ -1,2 +1,34 @@
+import com.mypackage.LinkedList;
 public class Main {
+    private static LinkedList list = new LinkedList();
+
+    public static void main(String[] args) {
+        list.put(123);
+        list.put("the string");
+        list.put(new Integer(456));
+        System.out.println("\nList elements");
+
+        for(int i=0;i<list.size();i++){
+            System.out.println("element "+i+" : "+list.get(i));
+        }
+        System.out.println("===============================");
+        System.out.println("List size : "+list.size());
+        System.out.println("Removing of element number 1");
+        list.remove(1);
+        System.out.println("\nList elements");
+        for(int i=0;i<list.size();i++){
+            System.out.println("element "+i+" : "+list.get(i));
+        }
+        System.out.println("================================");
+        System.out.println("List size : "+list.size());
+        System.out.println("\nPutting of element in the position number 1");
+        list.put("put element", 1);
+        System.out.println("\nList elements");
+        for(int i=0;i<list.size();i++){
+            System.out.println("element "+i+" : "+list.get(i));
+        }
+        System.out.println("================================");
+        System.out.println("List size : "+list.size());
+
+    }
 }
---
+++ Lesson3Task2/src/com/mypackage/LinkedList.java
@@ -0,0 +1,96 @@
+package com.mypackage;
+
+public class LinkedList {
+    private static int counter=0;
+    private Node head;
+
+    public void put(Object data){
+        if(head==null){
+            head = new Node(data);
+            }
+        Node temp = new Node(data);
+        Node current = head;
+
+        if (current!=null){
+            while(current.getNext()!=null){
+                current = current.getNext();
+            }
+        }
+        current.setNext(temp);
+        incrementCounter();
+    }
+
+    private static int getCounter(){
+        return counter;
+    }
+
+    private void incrementCounter(){
+        counter++;
+    }
+
+    private void decrementCounter(){
+        counter--;
+    }
+
+    public void put(Object data, int index){
+        Node temp = new Node(data);
+        Node current = head;
+
+        for(int i=0;i<index && current.getNext()!=null;i++){
+            current = current.getNext();
+        }
+
+        temp.setNext(current.getNext());
+        current.setNext(temp);
+        incrementCounter();
+    }
+
+    public Object get(int index) {
+        if (index < 0)
+            return null;
+        Node current = null;
+        if(head!=null){
+            current = head.getNext();
+
+            for (int i=0;i<index;i++){
+                if(current.getNext()==null) return null;
+                current = current.getNext();
+            }
+            return current.getData();
+        }
+        return current;
+    }
+
+    public boolean remove(int index){
+        if (index<1 || index>size()){
+            return false;
+        }
+        Node current = head;
+        if(head!=null){
+            for(int i=0;i<index;i++){
+                if(current.getNext()==null) return false;
+            }
+            decrementCounter();
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return getCounter();
+    }
+
+    public String toString(){
+        String output = "";
+        if(head!=null){
+            Node current = head.getNext();
+            while(current!=null){
+                output+="["+current.getData().toString()+"]";
+                current = current.getNext();
+            }
+        }
+        return output;
+    }
+}
+
+
